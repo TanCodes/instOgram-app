e4f9aa5525e9cb474464fdf994e09cb3242fd418
TanCodes <barvitanmay20@gmail.com>
2021-08-31 23:02:59 +0530

    initial commit

--- instogram/settings.py
+++ instogram/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-carkp9mq9p9k8o8b$ts^ep3y+)^tgna&ii+6@5-7b3#yx$$2nk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,7 +128,7 @@ STATICFILES_DIRS = [
 
 # django will put this all static files in assest
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
