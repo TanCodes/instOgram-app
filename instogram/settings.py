9cd4f7cb1a631888397f9bbf64e491df8b8bbd06
TanCodes <barvitanmay20@gmail.com>
2021-11-23 20:09:26 +0530

    updated

--- instogram/settings.py
+++ instogram/settings.py
@@ -4,6 +4,7 @@
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,15 +113,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-# django will put this all static files in assest
-STATIC_ROOT = os.path.join(BASE_DIR, 'instogram/staticfiles')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'instogram/static')
 ]
+
+# for Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
--- instogram/urls.py
+++ instogram/urls.py
@@ -6,10 +6,14 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
 
+# for heruko
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html")),
     path('Get_Post', views.Get_Post, name='Get_Post'),
     path('Get_Videos', views.Get_Videos, name='Get_Videos'),
     path('Get_Profile_Pic', views.Get_Profile_Pic, name='Get_Profile_Pic')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
