10fee1ba68bd6fd23a7d0b78f430b047ea9f3758
TanCodes <barvitanmay20@gmail.com>
2021-11-23 20:34:02 +0530

    updated

--- instogram/views.py
+++ instogram/views.py
@@ -40,6 +40,9 @@ def Get_Post(request):
             # output
         else:
             try:
+                header = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+                }
                 response = requests.get(PHOTO_URL, headers=header).json()
                 photo = response["graphql"]["shortcode_media"]["display_resources"]
                 photo = photo[2]["src"]
