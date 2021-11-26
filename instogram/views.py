9b117a9c92fe6b12e4f6553f49ea0a351c79f9e4
TANMAY BARVI <64544882+TanCodes@users.noreply.github.com>
2021-11-26 12:38:04 +0530

    Update views.py

--- instogram/views.py
+++ instogram/views.py
@@ -98,9 +98,11 @@ def Get_Videos(request):
 
 def Get_Profile_Pic(request):
     if request.method == "POST":
-        header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
-        }
+#         header = {
+#             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+#         }
+        
+        header = {'Accept': 'application/json'}
 
         # get message
         USER_NAME = request.POST['msg']
