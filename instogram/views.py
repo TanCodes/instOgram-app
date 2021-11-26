50b71fd1b1764ed900874b63d570f29bf079580a
TANMAY BARVI <64544882+TanCodes@users.noreply.github.com>
2021-11-26 12:30:30 +0530

    Update views.py

--- instogram/views.py
+++ instogram/views.py
@@ -99,7 +99,7 @@ def Get_Videos(request):
 def Get_Profile_Pic(request):
     if request.method == "POST":
         header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
         }
 
         # get message
